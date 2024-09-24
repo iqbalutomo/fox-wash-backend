07fd0b42ebcb996e3710fafb36a29adf9daa5453
yosia <yosialuther2803@gmail.com>
2024-09-24 22:13:24 +0700

    fix: fixing error response for crud of detailingpackage

---
+++ api_gateway/dto/washstation.go
@@ -0,0 +1,29 @@
+package dto
+
+type NewWashPackageData struct {
+	Name     string  `json:"name" validate:"required"`
+	Category uint32  `json:"category" validate:"required"`
+	Price    float64 `json:"price" validate:"required"`
+}
+
+type NewWashPackageResponse struct {
+	ID        uint32  `json:"id"`
+	Name      string  `json:"name"`
+	Category  uint32  `json:"category"`
+	Price     float64 `json:"price"`
+	CreatedBy uint32  `json:"created_by"`
+}
+
+type UpdateWashPackageData struct {
+	Name     string  `json:"name" validate:"required"`
+	Category uint32  `json:"category" validate:"required"`
+	Price    float64 `json:"price" validate:"required"`
+}
+
+type UpdateWashPackageResponse struct {
+	ID        uint32  `json:"id"`
+	Name      string  `json:"name"`
+	Category  uint32  `json:"category"`
+	Price     float64 `json:"price"`
+	CreatedBy uint32  `json:"created_by"`
+}
--- wash_station_service/repository/washstation_repository.go
+++ wash_station_service/repository/washstation_repository.go
@@ -6,6 +6,8 @@ import (
 	"wash_station_service/models"
 	"wash_station_service/pb"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 )
 
@@ -32,30 +34,80 @@ func NewWashStationRepository(db *gorm.DB) WashStation {
 }
 
 func (w *WashStationRepository) CreateWashPackage(data *models.Wash) error {
-	return errors.New("") // TODO: logic here
+	if err := w.db.Create(data).Error; err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	return nil
 }
 
 func (w *WashStationRepository) FindAllWashPackages() ([]models.Wash, error) {
-	return nil, errors.New("") // TODO: logic here
+	var washPackages []models.Wash
+
+	if err := w.db.Find(&washPackages).Error; err != nil {
+		return nil, err
+	}
+
+	return washPackages, nil
 }
 
 func (w *WashStationRepository) FindWashPackageByID(WashPackageID uint32) (dto.WashPackageDataCompact, error) {
-	return dto.WashPackageDataCompact{}, errors.New("") // TODO: logic here
+	var washPackage dto.WashPackageDataCompact
+
+	if err := w.db.Table("washes").Where("id = ?", WashPackageID).Take(&washPackage).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return dto.WashPackageDataCompact{}, status.Error(codes.NotFound, err.Error())
+		}
+
+		return dto.WashPackageDataCompact{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return washPackage, nil
 }
 
 func (w *WashStationRepository) UpdateWashPackage(WashPackageID uint32, data *pb.UpdateWashPackageData) error {
-	return errors.New("") // TODO: logic here
+	washPackage := models.Wash{ID: WashPackageID}
+
+	res := w.db.Model(&washPackage).Updates(models.Wash{
+		Name:     data.Name,
+		Category: data.Category,
+		Price:    float64(data.Price),
+	})
+	if err := res.Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return status.Error(codes.NotFound, err.Error())
+		}
+
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	return nil
 }
 
 func (w *WashStationRepository) DeleteWashPackage(WashPackageID uint32) error {
-	return errors.New("") // TODO: logic here
+	washPackageData := models.Wash{ID: WashPackageID}
+
+	res := w.db.Delete(&washPackageData, "id = ?", WashPackageID)
+	if err := res.Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return status.Error(codes.NotFound, err.Error())
+		}
+
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	if res.RowsAffected == 0 {
+		return status.Error(codes.NotFound, "invalid wash package ID")
+	}
+
+	return nil
 }
 
 //detailing package repo
 
 func (w *WashStationRepository) CreateDetailingPackage(data *models.Detailing) error {
 	if err := w.db.Create(data).Error; err != nil {
-		return err
+		return status.Error(codes.Internal, err.Error())
 	}
 	return nil
 }
@@ -71,7 +123,11 @@ func (w *WashStationRepository) FindAllDetailingPackages() ([]models.Detailing, 
 func (w *WashStationRepository) FindDetailingPackageByID(DetailingPackageID uint32) (dto.DetailingPackageDataCompact, error) {
 	var detailing models.Detailing
 	if err := w.db.First(&detailing, DetailingPackageID).Error; err != nil {
-		return dto.DetailingPackageDataCompact{}, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return dto.DetailingPackageDataCompact{}, status.Error(codes.NotFound, err.Error())
+		}
+
+		return dto.DetailingPackageDataCompact{}, status.Error(codes.Internal, err.Error())
 	}
 	return dto.DetailingPackageDataCompact{
 		ID:          detailing.ID,
@@ -83,14 +139,22 @@ func (w *WashStationRepository) FindDetailingPackageByID(DetailingPackageID uint
 
 func (w *WashStationRepository) UpdateDetailingPackage(DetailingPackageID uint32, data *pb.UpdateDetailingPackageData) error {
 	if err := w.db.Model(&models.Detailing{}).Where("id = ?", DetailingPackageID).Updates(data).Error; err != nil {
-		return err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return status.Error(codes.NotFound, err.Error())
+		}
+
+		return status.Error(codes.Internal, err.Error())
 	}
 	return nil
 }
 
 func (w *WashStationRepository) DeleteDetailingPackage(DetailingPackageID uint32) error {
 	if err := w.db.Delete(&models.Detailing{}, DetailingPackageID).Error; err != nil {
-		return err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return status.Error(codes.NotFound, err.Error())
+		}
+
+		return status.Error(codes.Internal, err.Error())
 	}
 	return nil
 }

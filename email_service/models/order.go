290fde58e765d77896ab7cf8e1fddcffcc097804
iqbalutomo <muhlisiqbalutomo@gmail.com>
2024-09-26 00:40:59 +0700

    feat: format rupiah

--- email_service/models/order.go
+++ email_service/models/order.go
@@ -12,26 +12,26 @@ type Order struct {
 type OrderDetail struct {
 	WashPackage      []WashPackage      `json:"wash_packages"`
 	DetailingPackage []DetailingPackage `json:"detailing_packages"`
-	AppFee           float32            `json:"app_fee"`
-	TotalPrice       float32            `json:"total_price"`
+	AppFee           string             `json:"app_fee"`
+	TotalPrice       string             `json:"total_price"`
 }
 
 type WashPackage struct {
-	ID       int     `json:"id"`
-	Name     string  `json:"name"`
-	Category int     `json:"category"`
-	Price    float32 `json:"price"`
-	Qty      uint    `json:"qty"`
-	SubTotal float32 `json:"subtotal"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Category int    `json:"category"`
+	Price    string `json:"price"`
+	Qty      uint   `json:"qty"`
+	SubTotal string `json:"subtotal"`
 }
 
 type DetailingPackage struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	Qty         uint    `json:"qty"`
-	SubTotal    float32 `json:"subtotal"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       string `json:"price"`
+	Qty         uint   `json:"qty"`
+	SubTotal    string `json:"subtotal"`
 }
 
 type User struct {
--- order_service/dto/order.go
+++ order_service/dto/order.go
@@ -7,3 +7,28 @@ type OrderCalculateResponse struct {
 	AppFee           float32              `bson:"app_fee" json:"app_fee"`
 	TotalPrice       float32              `bson:"total_price" json:"total_price"`
 }
+
+type MbOrderDetail struct {
+	WashPackage      []MbWashPackage      `json:"wash_packages"`
+	DetailingPackage []MbDetailingPackage `json:"detailing_packages"`
+	AppFee           string               `json:"app_fee"`
+	TotalPrice       string               `json:"total_price"`
+}
+
+type MbWashPackage struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Category int    `json:"category"`
+	Price    string `json:"price"`
+	Qty      uint   `json:"qty"`
+	SubTotal string `json:"subtotal"`
+}
+
+type MbDetailingPackage struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       string `json:"price"`
+	Qty         uint   `json:"qty"`
+	SubTotal    string `json:"subtotal"`
+}
